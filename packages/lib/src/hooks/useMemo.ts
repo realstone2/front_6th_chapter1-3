22036543f52fbd8876dfa192a24f6c2e04b3428d
진돌 <wlstjrghdud@naver.com>
2025-07-21 21:58:14 +0900

    feat(lib): useMemo 훅 구현
    - useMemo 훅을 구현하여 메모이제이션 기능 추가
    - 의존성 배열과 비교를 위한 shallowEquals 사용

--- packages/lib/src/hooks/useMemo.ts
+++ packages/lib/src/hooks/useMemo.ts
@@ -1,8 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { DependencyList } from "react";
 import { shallowEquals } from "../equals";
+import { useRef } from "./useRef";
 
 export function useMemo<T>(factory: () => T, _deps: DependencyList, _equals = shallowEquals): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+  const memoizedDeps = useRef<DependencyList>(_deps);
+
+  const memoizedValue = useRef<T | null>(null);
+
+  if (!_equals(memoizedDeps.current, _deps) || memoizedValue.current === null) {
+    memoizedValue.current = factory();
+  }
+
+  memoizedDeps.current = _deps;
+
+  return memoizedValue.current;
 }
