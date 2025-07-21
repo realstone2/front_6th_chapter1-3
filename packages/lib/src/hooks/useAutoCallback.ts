af43896f81c63a126603f5879cb6d5b77d6abadb
진돌 <wlstjrghdud@naver.com>
2025-07-21 23:29:17 +0900

    feat(lib): useAutoCallback 훅 구현
    - 함수 참조를 유지하는 useAutoCallback 훅 추가
    - useRef와 useCallback을 활용하여 메모이제이션 처리

--- packages/lib/src/hooks/useAutoCallback.ts
+++ packages/lib/src/hooks/useAutoCallback.ts
@@ -2,6 +2,13 @@ import type { AnyFunction } from "../types";
 import { useCallback } from "./useCallback";
 import { useRef } from "./useRef";
 
-export const useAutoCallback = <T extends AnyFunction>(fn: T): T => {
-  return fn;
+export const useAutoCallback = <T extends AnyFunction>(fn: T) => {
+  const callbackRef = useRef<T>(fn);
+  callbackRef.current = fn;
+
+  const memoCallback = useCallback((...args: Parameters<T>) => {
+    return callbackRef.current(...args);
+  }, []);
+
+  return memoCallback;
 };
