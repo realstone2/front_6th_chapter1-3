e44c0d1061194790ddbdb50b59c0fe84f2492cd9
진돌 <wlstjrghdud@naver.com>
2025-07-21 22:33:59 +0900

    feat(lib): useShallowState 훅 구현
    - useState를 사용하여 상태를 관리하고 shallowEquals를 통해 상태 변경을 감지하는 기능 추가

--- packages/lib/src/hooks/useShallowState.ts
+++ packages/lib/src/hooks/useShallowState.ts
@@ -1,7 +1,18 @@
 import { useState } from "react";
 import { shallowEquals } from "../equals";
+import { useCallback } from "./useCallback";
 
-export const useShallowState = <T>(initialValue: Parameters<typeof useState<T>>[0]) => {
+export const useShallowState = <T>(initialValue: T) => {
   // useState를 사용하여 상태를 관리하고, shallowEquals를 사용하여 상태 변경을 감지하는 훅을 구현합니다.
-  return useState(initialValue);
+
+  const [state, setState] = useState<T>(initialValue);
+
+  const setShallowState = useCallback((newValue: T) => {
+    if (!shallowEquals(state, newValue)) {
+      setState(newValue);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return [state, setShallowState] as const;
 };
