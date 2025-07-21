057ef59eafa33c81bd45764ad1dda99fba332c9d
진돌 <wlstjrghdud@naver.com>
2025-07-21 22:01:50 +0900

    feat(lib): useCallback 훅 구현
    - useCallback 훅을 구현하여 함수 메모이제이션 기능 추가
    - shallowEquals를 사용하여 의존성 비교 로직 추가

--- packages/lib/src/hooks/useCallback.ts
+++ packages/lib/src/hooks/useCallback.ts
@@ -1,7 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/no-unsafe-function-type */
 import type { DependencyList } from "react";
+import { useRef } from "./useRef";
+import { shallowEquals } from "../equals";
 
 export function useCallback<T extends Function>(factory: T, _deps: DependencyList) {
-  // 직접 작성한 useMemo를 통해서 만들어보세요.
-  return factory as T;
+  const memoizedDeps = useRef<DependencyList>(_deps);
+
+  const memoizedValue = useRef<T | null>(null);
+
+  if (!shallowEquals(memoizedDeps.current, _deps) || memoizedValue.current === null) {
+    memoizedValue.current = factory;
+  }
+
+  memoizedDeps.current = _deps;
+
+  return memoizedValue.current;
 }
